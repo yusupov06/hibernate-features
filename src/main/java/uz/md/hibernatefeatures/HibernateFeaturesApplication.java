0e719427a6eb995bcb1a24c0cb16f3094dadcac2
yusupov06 <yusupovforwin@gmail.com>
2023-05-14 16:04:14 +0500

    init

---
+++ src/main/java/uz/md/hibernatefeatures/HibernateFeaturesApplication.java
@@ -0,0 +1,13 @@
+package uz.md.hibernatefeatures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateFeaturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateFeaturesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/md/hibernatefeatures/HibernateFeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.md.hibernatefeatures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateFeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
